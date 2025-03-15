5b35eb2ff88c2f8a0efab974d100fff2b2fe9ebe
LRLayan <rameshlayan4@gmail.com>
2025-03-16 03:08:48 +0530

    modify root layout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
-import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-} from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined, } from '@ant-design/icons';
 import { FiMenu } from "react-icons/fi";
 import { Button, Layout, Menu, theme } from 'antd';
 import {useState} from "react";
+import * as Tabs from "@radix-ui/react-tabs";
+import {Box, Flex} from "@radix-ui/themes";
+import ChatCard from "./chatCard/chatCard.tsx";
 
 const { Header, Sider, Content } = Layout;
+
+const chatDetails = [
+    {}
+];
+
 
 const RootLayout: React.FC = () => {
     const [collapsed, setCollapsed] = useState(false);
@@ -20,7 +22,7 @@ const RootLayout: React.FC = () => {
 
     return (
         <Layout>
-            <Sider trigger={null} collapsible collapsed={collapsed} width={388} style={{ background: "#1f252e" }}>
+            <Sider trigger={null} collapsible collapsed={collapsed} width={388} collapsedWidth={120} style={{ background: "#1f252e" }}>
                 <div className="demo-logo-vertical" />
                 <Button
                     type="text"
@@ -32,6 +34,23 @@ const RootLayout: React.FC = () => {
                         color: "white",
                     }}
                 />
+                <Flex direction="column" className="gap-4 pb-2 justify-center">
+                    <Tabs.Root defaultValue="allChats">
+                        <Tabs.List className="text-white border-b border-gray-700">
+                            <Tabs.Trigger value="allChats" className="px-4 py-2 border-b-2 border-transparent hover:border-gray-500 data-[state=active]:border-cyan-500">All Chats</Tabs.Trigger>
+                            <Tabs.Trigger value="Groups" className="px-4 py-2 border-b-2 border-transparent hover:border-gray-500 data-[state=active]:border-cyan-500">Groups</Tabs.Trigger>
+                        </Tabs.List>
+
+                        <Box pt="3">
+                            {["allChats", "Groups"].map((category) => (
+                                <Tabs.Content key={category} value={category}>
+                                    {collapsed ? <ChatCard collapse={collapsed}/> : <ChatCard collapse={collapsed}/>
+                                    }
+                                </Tabs.Content>
+                            ))}
+                        </Box>
+                    </Tabs.Root>
+                </Flex>
                 <Menu
                     theme="dark"
                     mode="vertical"
