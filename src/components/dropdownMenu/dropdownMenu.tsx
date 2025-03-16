3a8b44996d5c7e6c22eb8836ada61ab37f96c876
LRLayan <rameshlayan4@gmail.com>
2025-03-16 21:08:10 +0530

    dropdown menu

---
+++ src/components/dropdownMenu/dropdownMenu.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { FiMenu } from "react-icons/fi";
+import { Button, } from 'antd';
+import { DropdownMenu} from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import { User, Bookmark, Archive, Star, Settings, } from "lucide-react";
+
+const DropdownMenuSet: React.FC = () => {
+    return (
+        <>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                    <Button
+                        type="text"
+                        icon={<FiMenu size={27}/>}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                            color: "white",
+                        }}
+                    />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                    <DropdownMenu.Item>
+                        <User className="w-4 h-4 mr-2" /> Profile
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item>
+                        <Bookmark className="w-4 h-4 mr-2" /> Saved Message
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Separator />
+                    <DropdownMenu.Item>
+                        <Archive className="w-4 h-4 mr-2" /> Archive Chats
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item>
+                        <Star className="w-4 h-4 mr-2" /> Add to favorites
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Separator />
+                    <DropdownMenu.Sub>
+                        <DropdownMenu.Item>
+                            <Settings className="w-4 h-4 mr-2" /> Setting
+                        </DropdownMenu.Item>
+                        <DropdownMenu.SubTrigger>More</DropdownMenu.SubTrigger>
+                        <DropdownMenu.SubContent>
+                            <DropdownMenu.Item>Move to project…</DropdownMenu.Item>
+                            <DropdownMenu.Item>Move to folder…</DropdownMenu.Item>
+
+                            <DropdownMenu.Separator />
+                            <DropdownMenu.Item>Advanced options…</DropdownMenu.Item>
+                        </DropdownMenu.SubContent>
+                    </DropdownMenu.Sub>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
+        </>
+    )
+}
+
+export default DropdownMenuSet;
