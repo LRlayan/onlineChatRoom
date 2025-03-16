cacda1a3ff327b218068a2535bafe316fb4f6bad
LRLayan <rameshlayan4@gmail.com>
2025-03-17 04:11:17 +0530

    setShowProfile

--- src/components/dropdownMenu/dropdownMenu.tsx
+++ src/components/dropdownMenu/dropdownMenu.tsx
@@ -5,54 +5,37 @@ import { DropdownMenu} from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { User, Bookmark, Archive, Star, Settings, } from "lucide-react";
 
-const DropdownMenuSet: React.FC = () => {
+const DropdownMenuSet: React.FC<{ setShowProfile: (value: boolean) => void }> = ({ setShowProfile }) => {
     return (
-        <>
-            <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                    <Button
-                        type="text"
-                        icon={<FiMenu size={27}/>}
-                        style={{
-                            fontSize: '16px',
-                            width: 64,
-                            height: 64,
-                            color: "white",
-                        }}
-                    />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                    <DropdownMenu.Item>
-                        <User className="w-4 h-4 mr-2" /> Profile
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item>
-                        <Bookmark className="w-4 h-4 mr-2" /> Saved Message
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Separator />
-                    <DropdownMenu.Item>
-                        <Archive className="w-4 h-4 mr-2" /> Archive Chats
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item>
-                        <Star className="w-4 h-4 mr-2" /> Add to favorites
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Separator />
-                    <DropdownMenu.Sub>
-                        <DropdownMenu.Item>
-                            <Settings className="w-4 h-4 mr-2" /> Setting
-                        </DropdownMenu.Item>
-                        <DropdownMenu.SubTrigger>More</DropdownMenu.SubTrigger>
-                        <DropdownMenu.SubContent>
-                            <DropdownMenu.Item>Move to project…</DropdownMenu.Item>
-                            <DropdownMenu.Item>Move to folder…</DropdownMenu.Item>
-
-                            <DropdownMenu.Separator />
-                            <DropdownMenu.Item>Advanced options…</DropdownMenu.Item>
-                        </DropdownMenu.SubContent>
-                    </DropdownMenu.Sub>
-                </DropdownMenu.Content>
-            </DropdownMenu.Root>
-        </>
-    )
-}
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+                <Button
+                    type="text"
+                    icon={<FiMenu size={27} />}
+                    style={{ fontSize: '16px', width: 64, height: 64, color: "white" }}
+                />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+                <DropdownMenu.Item onClick={() => setShowProfile(true)}>
+                    <User className="w-4 h-4 mr-2" /> Profile
+                </DropdownMenu.Item>
+                <DropdownMenu.Item>
+                    <Bookmark className="w-4 h-4 mr-2" /> Saved Message
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator />
+                <DropdownMenu.Item>
+                    <Archive className="w-4 h-4 mr-2" /> Archive Chats
+                </DropdownMenu.Item>
+                <DropdownMenu.Item>
+                    <Star className="w-4 h-4 mr-2" /> Add to favorites
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator />
+                <DropdownMenu.Item>
+                    <Settings className="w-4 h-4 mr-2" /> Setting
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
+    );
+};
 
 export default DropdownMenuSet;
