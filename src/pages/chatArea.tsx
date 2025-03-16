117cf42f5a552efcbcc9b71d441d9317a449924d
LRLayan <rameshlayan4@gmail.com>
2025-03-16 16:18:40 +0530

    chat area

---
+++ src/pages/chatArea.tsx
@@ -0,0 +1,9 @@
+export function ChatArea() {
+
+    return(
+        <>
+            <p className="bg-blue-500 bg-opacity-90 p-5 rounded-lg text-amber-50 w-fit mb-2">Chat Area</p>
+            <p className="bg-blue-500 bg-opacity-90 p-5 rounded-lg text-amber-50 w-fit">Chat Area Develop</p>
+        </>
+    )
+}
