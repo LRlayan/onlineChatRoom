e0067df596ed6bc42ec4279cdb9417e00831b805
LRLayan <rameshlayan4@gmail.com>
2025-03-18 05:23:21 +0530

    segmentSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import SegmentsSlice from "../reducer/segmentsSlice.ts";
+
 
 export const store = configureStore({
     reducer:{
-
+        segment: SegmentsSlice,
     }
 });
 
