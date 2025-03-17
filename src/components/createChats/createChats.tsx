2b6248ef7e34d01fb3b696f0bf57f2d4f59a1f29
LRLayan <rameshlayan4@gmail.com>
2025-03-18 05:20:42 +0530

    modify and bug fix

--- src/components/createChats/createChats.tsx
+++ src/components/createChats/createChats.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ArrowLeft} from "lucide-react";
 import {Button, type GetProp, Upload, type UploadFile, type UploadProps} from "antd";
-import {Flex, Text, Tooltip, TextField, Box} from "@radix-ui/themes";
+import {Flex, Text, Tooltip,} from "@radix-ui/themes";
 import ImgCrop from "antd-img-crop";
 import {
     PhoneOutlined,
@@ -10,13 +10,17 @@ import {
 } from '@ant-design/icons';
 import { Segmented } from 'antd';
 import ContactCard from "../contactCard/contactCard.tsx";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "../../store/store.ts";
+import {changeSegment} from "../../reducer/segments.ts";
+import RoomDataForm from "./roomDataForm/roomDataForm.tsx";
 
 interface CreateRoomsProps {
     collapse: boolean;
     setShowCreateRooms: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const contactDetails = [
+export const contactDetails = [
     {name: "Amodh", email: "amodh@gmail.com", bio: "Software", profile: ""},
     {name: "Kavindu", email: "kavi@gmail.com", bio: "Software Dev", profile: ""},
     {name: "Tharusha", email: "capa@gmail.com", bio: "Software Engineering", profile: ""},
@@ -29,8 +33,16 @@ const contactDetails = [
 type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
 
 const CreateChats: React.FC<CreateRoomsProps> = ({ setShowCreateRooms, collapse }) => {
-
     const [selectedValue, setSelectedValue] = useState("New Contact");
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        if (selectedValue === "New Rooms") {
+            dispatch(changeSegment({ segment: "New Rooms" , collapse: true}));
+        } else if (collapse === false && selectedValue === "New Contact") {
+            dispatch(changeSegment({ segment: "New Contact", collapse: false }));
+        }
+    }, [selectedValue]);
 
     const handleArrowBtn = () => {
         setShowCreateRooms(false);
@@ -84,13 +96,13 @@ const CreateChats: React.FC<CreateRoomsProps> = ({ setShowCreateRooms, collapse 
     return(
         <>
             <div className="pl-4 pt-4 text-white">
-                <Flex className="flex pr-4" style={{justifyContent: "space-between", alignItems: "center", width: "100%"}}>
+                <Flex className="flex pr-" style={{justifyContent: "space-between", alignItems: "center", width: "100%"}}>
                     <Button type="text" icon={<ArrowLeft/>} onClick={handleArrowBtn}
                             style={{fontSize: '20px', color: "white", alignSelf: "start"}}
                     />
                     <Text style={{fontSize: "24px", font: "icon", display: collapse ? 'none' : ''}}>New Chat</Text>
                     <Tooltip content="Add To Contact">
-                        <Button type="text" icon={<PhoneOutlined color="white" className="text-amber-50" style={{ fontSize: "25px" }}/>} onClick={handleAddContact}/>
+                        <Button type="text" icon={<PhoneOutlined color="white" className="text-amber-50 mr-8" style={{ fontSize: "25px" }}/>} onClick={handleAddContact}/>
                     </Tooltip>
                 </Flex>
                 <Flex justify="center" align="center" className="flex flex-col mt-5 mb-7 text-center">
@@ -129,38 +141,7 @@ const CreateChats: React.FC<CreateRoomsProps> = ({ setShowCreateRooms, collapse 
                                     </Upload>
                                 </ImgCrop>
                             </Flex>
-                            <Flex gapY={"1"} className="flex flex-col ml-0 bg-gray-500 bg-opacity-25 rounded-lg pr-5 pl-5 pt-0 pb-3 mr-4">
-                                <Flex align="center" className="items-center pr-0 pl-0 pt-3 rounded-lg">
-                                    <Flex direction="column" gap="1" maxWidth="300px">
-                                        <Text className="text-gray-400" style={{fontSize: "12px", font: "revert", textAlign: "start"}}>Room Name</Text>
-                                        <TextField.Root size="2" variant="soft" placeholder="room name" style={{ width: "250px" }} />
-                                    </Flex>
-                                </Flex>
-                                <Text className="text-gray-400" style={{fontSize: "12px", font: "revert", textAlign: "start"}}>Select Contact</Text>
-                                <Box
-                                    style={{
-                                        height: "39vh",
-                                        overflowY: "auto",
-                                        overflowX: "hidden",
-                                        scrollBehavior: "smooth",
-                                        scrollbarWidth: "thin",
-                                        scrollbarColor: "#888 #333",
-                                    }}>
-                                    {contactDetails.map((contact, index) => {
-                                        return (
-                                            <ContactCard
-                                                key={index}
-                                                collapse={collapse}
-                                                selectedSegment={selectedValue}
-                                                name={contact.name}
-                                                bio={contact.bio}
-                                                profile={contact.profile}
-                                                onClick={() => handleContactCard(contact)}
-                                            />
-                                        );
-                                    })}
-                                </Box>
-                            </Flex>
+                            <RoomDataForm collapse={collapse} selectedValue={selectedValue} />
                         </>
                     ) : (
                             <div className="w-full pr-4" style={{
@@ -190,6 +171,36 @@ const CreateChats: React.FC<CreateRoomsProps> = ({ setShowCreateRooms, collapse 
                     }
                 </Flex>
             </div>
+
+            {/* Create Rooms Modal - if collapse */}
+            {/*{ modalOpen &&*/}
+            {/*    <ConfigProvider*/}
+            {/*        theme={{*/}
+            {/*            token: {*/}
+            {/*                colorBgElevated: "",*/}
+            {/*                colorText: "#ffffff",*/}
+            {/*                colorPrimary: "",*/}
+            {/*            },*/}
+            {/*        }}*/}
+            {/*    >*/}
+            {/*        <Modal*/}
+            {/*            title="Created Rooms"*/}
+            {/*            centered*/}
+            {/*            className="dark-modal"*/}
+            {/*            open={modalOpen}*/}
+            {/*            onOk={() => setModalOpen(false)}*/}
+            {/*            onCancel={() => setModalOpen(false)}*/}
+            {/*            style={{*/}
+            {/*                background: "#131517",*/}
+            {/*                color:"#ffffff",*/}
+            {/*                borderRadius: "12px",*/}
+            {/*                overflow: "hidden",*/}
+            {/*            }}*/}
+            {/*        >*/}
+
+            {/*        </Modal>*/}
+            {/*    </ConfigProvider>*/}
+            {/*}*/}
         </>
     )
 }
