42f1c79a204ae06a9544e4f00990760b347deb1b
LRLayan <rameshlayan4@gmail.com>
2025-03-17 18:58:21 +0530

    add fragment

--- src/components/profileView/profileView.tsx
+++ src/components/profileView/profileView.tsx
@@ -56,60 +56,64 @@ const ProfileView: React.FC<ProfileViewProps> = ({ setShowProfile, collapse }) =
     const handleEditProfile = () => {}
 
     return (
-        <div className="p-4 text-white">
-            <Flex className="flex" style={{ justifyContent: "space-between", alignItems: "center", width: "100%" }}>
-                <Button type="text" icon={<ArrowLeft />} onClick={handleArrowBtn}
-                        style={{ fontSize: '20px', color: "white", alignSelf: "start" }}
-                />
-                <Text style={{ fontSize: "24px", font: "icon", display: collapse ? 'none' : '' }}>Profile</Text>
-                <Button type="text" icon={<Pencil color="white"/>} onClick={handleEditProfile}/>
-            </Flex>
+        <>
+            <div className="p-4 text-white">
+                <Flex className="flex" style={{justifyContent: "space-between", alignItems: "center", width: "100%"}}>
+                    <Button type="text" icon={<ArrowLeft/>} onClick={handleArrowBtn}
+                            style={{fontSize: '20px', color: "white", alignSelf: "start"}}
+                    />
+                    <Text style={{fontSize: "24px", font: "icon", display: collapse ? 'none' : ''}}>Profile</Text>
+                    <Button type="text" icon={<Pencil color="white"/>} onClick={handleEditProfile}/>
+                </Flex>
 
-            <Flex justify="center" align="center" className="flex flex-col mt-7 mb-7 text-center">
-                <ImgCrop rotationSlider>
-                    <Upload
-                        action="http://localhost:3000/upload-image"
-                        listType="picture-circle"
-                        fileList={fileList}
-                        onChange={onChange}
-                        onPreview={onPreview}
-                        beforeUpload={handleBeforeUpload}
-                        className="text-blue-500"
-                    >
-                        {fileList.length < 1 && '+ Upload'}
-                    </Upload>
-                </ImgCrop>
-                <Text className="mt-5 mb-1" style={{ fontSize: "18px", font: "icon" }}>Ramesh</Text>
-                <Text className="text-gray-400" style={{ fontSize: "12px", font: "icon" }}>online</Text>
-            </Flex>
+                <Flex justify="center" align="center" className="flex flex-col mt-7 mb-7 text-center">
+                    <ImgCrop rotationSlider>
+                        <Upload
+                            action="http://localhost:3000/upload-image"
+                            listType="picture-circle"
+                            fileList={fileList}
+                            onChange={onChange}
+                            onPreview={onPreview}
+                            beforeUpload={handleBeforeUpload}
+                            className="text-blue-500"
+                        >
+                            {fileList.length < 1 && '+ Upload'}
+                        </Upload>
+                    </ImgCrop>
+                    <Text className="mt-5 mb-1" style={{fontSize: "18px", font: "icon"}}>Ramesh</Text>
+                    <Text className="text-gray-400" style={{fontSize: "12px", font: "icon"}}>online</Text>
+                </Flex>
 
-            <Flex gapY={"1"} className="flex flex-col ml-0 bg-gray-500 bg-opacity-25 rounded-lg pr-5 pl-5 pt-3 pb-3">
-                <Flex align="center" className="items-center hover:bg-blue-400 hover:bg-opacity-25 p-3 rounded-lg">
-                    <UserOutlined className="text-gray-400" style={{ fontSize: "25px", marginRight: "12px" }} />
-                    <Flex className="flex flex-col ml-6" style={{ display: collapse ? 'none' : '' }}>
-                        <Text style={{ fontSize: "18px", font: "icon" }}>rameshlayan</Text>
-                        <Text className="mb-0 text-gray-400" style={{ fontSize: "12px", font: "revert" }}>Username</Text>
+                <Flex gapY={"1"}
+                      className="flex flex-col ml-0 bg-gray-500 bg-opacity-25 rounded-lg pr-5 pl-5 pt-3 pb-3">
+                    <Flex align="center" className="items-center hover:bg-blue-400 hover:bg-opacity-25 p-3 rounded-lg">
+                        <UserOutlined className="text-gray-400" style={{fontSize: "25px", marginRight: "12px"}}/>
+                        <Flex className="flex flex-col ml-6" style={{display: collapse ? 'none' : ''}}>
+                            <Text style={{fontSize: "18px", font: "icon"}}>rameshlayan</Text>
+                            <Text className="mb-0 text-gray-400"
+                                  style={{fontSize: "12px", font: "revert"}}>Username</Text>
+                        </Flex>
+                    </Flex>
+                    <DropdownMenu.Separator/>
+                    <Flex align="center" className="items-center hover:bg-blue-400 hover:bg-opacity-25 p-3 rounded-lg">
+                        <IdcardOutlined className="text-gray-400" style={{fontSize: "25px", marginRight: "12px"}}/>
+                        <Flex className="flex flex-col ml-6" style={{display: collapse ? 'none' : ''}}>
+                            <Text style={{fontSize: "18px", font: "icon"}}>Ramesh Layan</Text>
+                            <Text className="mb-0 text-gray-400" style={{fontSize: "12px", font: "revert"}}>Name</Text>
+                        </Flex>
+                    </Flex>
+                    <DropdownMenu.Separator/>
+                    <Flex align="center" className="items-center hover:bg-blue-400 hover:bg-opacity-25 p-3 rounded-lg">
+                        <MailOutlined className="text-gray-400" style={{fontSize: "25px", marginRight: "12px"}}/>
+                        <Flex className="flex flex-col ml-6" style={{display: collapse ? 'none' : ''}}>
+                            <Text style={{fontSize: "18px", font: "icon"}}>rameshlayan4@gmail.com</Text>
+                            <Text className="text-gray-400" style={{fontSize: "12px", font: "revert"}}>Email</Text>
+                        </Flex>
                     </Flex>
                 </Flex>
-                <DropdownMenu.Separator/>
-                <Flex align="center" className="items-center hover:bg-blue-400 hover:bg-opacity-25 p-3 rounded-lg">
-                    <IdcardOutlined className="text-gray-400"  style={{ fontSize: "25px", marginRight: "12px" }} />
-                    <Flex className="flex flex-col ml-6" style={{ display: collapse ? 'none' : '' }}>
-                        <Text style={{ fontSize: "18px", font: "icon" }}>Ramesh Layan</Text>
-                        <Text className="mb-0 text-gray-400" style={{ fontSize: "12px", font: "revert" }}>Name</Text>
-                    </Flex>
-                </Flex>
-                <DropdownMenu.Separator/>
-                <Flex align="center" className="items-center hover:bg-blue-400 hover:bg-opacity-25 p-3 rounded-lg">
-                    <MailOutlined className="text-gray-400"  style={{ fontSize: "25px", marginRight: "12px" }} />
-                    <Flex className="flex flex-col ml-6" style={{ display: collapse ? 'none' : '' }}>
-                        <Text style={{ fontSize: "18px", font: "icon" }}>rameshlayan4@gmail.com</Text>
-                        <Text className="text-gray-400" style={{ fontSize: "12px", font: "revert" }}>Email</Text>
-                    </Flex>
-                </Flex>
-            </Flex>
-            <Button className="mt-3 bg-red-700 border-0 shadow-md text-white w-full">Logout</Button>
-        </div>
+                <Button className="mt-3 bg-red-700 border-0 shadow-md text-white w-full">Logout</Button>
+            </div>
+        </>
     );
 };
 
