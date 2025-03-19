60398effed7c7b192b41c464dbd8a172a1c210ef
LRLayan <rameshlayan4@gmail.com>
2025-03-19 22:08:45 +0530

    add RoomSlice to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import SegmentsSlice from "../reducer/segmentsSlice.ts";
+import RoomSlice from "../reducer/roomSlice.ts";
 
 
 export const store = configureStore({
     reducer:{
         segment: SegmentsSlice,
+        rooms: RoomSlice,
     }
 });
 
