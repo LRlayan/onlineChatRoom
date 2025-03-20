96666171b5736cdbff87e076fc4a0ae038393bcc
LRLayan <rameshlayan4@gmail.com>
2025-03-20 15:40:59 +0530

    re modify room schema

---
+++ schema/room.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export interface IRooms {
+    roomCode: string | null;
+    name: string;
+    createAt: Date;
+    image: string | null;
+    members: mongoose.Types.ObjectId[];
+}
+
+const rooms = new mongoose.Schema<IRooms>({
+    roomCode: String,
+    name: String,
+    createAt: { type: Date, default: Date.now },
+    image: String,
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "Contacts" }],
+});
+
+const Rooms = mongoose.model<any>("Rooms", rooms);
+export default Rooms;
