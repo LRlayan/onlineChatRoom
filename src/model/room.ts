a46d32921de1188595cd805852158da5842b5b18
LRLayan <rameshlayan4@gmail.com>
2025-03-20 18:40:06 +0530

    modify data type

--- src/model/room.ts
+++ src/model/room.ts
@@ -1,11 +1,15 @@
+import {Contact} from "./contact.ts";
+
 export class Room {
     name: string;
-    createAt: Date;
-    members: [];
+    createAt: number | undefined;
+    image: File | string | null;
+    members: Contact[];
 
-    constructor(name: string, createAt: Date, members: []) {
+    constructor(name: string, createAt: number | undefined, image: File | string | null, members: Contact[]) {
         this.name = name;
         this.createAt = createAt;
+        this.image = image;
         this.members = members;
     }
 }
