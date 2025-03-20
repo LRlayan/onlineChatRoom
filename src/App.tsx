cb89a066c7ca59e0df794d845eb43bb9ea74acb4
LRLayan <rameshlayan4@gmail.com>
2025-03-21 03:13:14 +0530

    apply authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,29 @@ import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router";
 import RootLayout from "./components/RootLayout.tsx";
 import { Theme } from "@radix-ui/themes";
+import SignInSignUp from "./pages/signIn&signUp.tsx";
+import {useSelector} from "react-redux";
+import {UserRootState} from "./reducer/userSlice.ts";
 
 function App() {
+    const isAuthenticated = useSelector((state: UserRootState) => state.user.isAuthenticated);
 
-  const routes = createBrowserRouter([
-      {
-      path: '/',
-        element: <RootLayout/>,
-          children:[
+    const routes = createBrowserRouter([
+        {path: '/', element: <SignInSignUp/> },
+        {
+            path: '/dashboard',
+            element: isAuthenticated ? <RootLayout/> : <SignInSignUp/>,
+            children:[]
+        }
+    ]);
 
-          ]
-    }
-  ]);
-
-  return (
-    <>
-        <Theme appearance="dark">
-            <RouterProvider router={routes}/>
-        </Theme>
-    </>
-  )
+    return (
+        <>
+            <Theme appearance="dark">
+                <RouterProvider router={routes}/>
+            </Theme>
+        </>
+    )
 }
 
 export default App
