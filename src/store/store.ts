ab74d456cd2fb3e17045a5b2c8f393ec0ec5ea8e
LRLayan <rameshlayan4@gmail.com>
2025-03-21 03:14:07 +0530

    userSlice set in to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import SegmentsSlice from "../reducer/segmentsSlice.ts";
 import RoomSlice from "../reducer/roomSlice.ts";
+import UserSlice from "../reducer/userSlice.ts";
 
 
 export const store = configureStore({
     reducer:{
         segment: SegmentsSlice,
         rooms: RoomSlice,
+        user: UserSlice,
     }
 });
 
