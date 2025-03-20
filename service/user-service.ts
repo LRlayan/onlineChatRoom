b766fd8bd68608d6e5139afd357411d804c70ed9
LRLayan <rameshlayan4@gmail.com>
2025-03-20 23:20:38 +0530

    saveUserService()

--- service/user-service.ts
+++ service/user-service.ts
@@ -1,4 +1,14 @@
-import {verifyUserCredentials} from "../repository/user-repository";
+import {saveUserRepository, verifyUserCredentials} from "../repository/user-repository";
+import {UserModel} from "../model/user-model";
+
+export async function saveUserService(user: UserModel) {
+    try {
+        const users = new UserModel(user.username, user.email, user.password);
+        return await saveUserRepository(users);
+    } catch (e) {
+        throw e;
+    }
+}
 
 export async function verifyUserCredentialsService(username: string, password: string) {
     try {
