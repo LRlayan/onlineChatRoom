0f3db386310c7aea946f720b30ebe895211b2237
LRLayan <rameshlayan4@gmail.com>
2025-03-21 03:16:16 +0530

    relation

--- schema/user.ts
+++ schema/user.ts
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
 
-const User = new mongoose.Schema({
+export interface IUser {
+    username: string;
+    email: string;
+    password: string;
+    contacts: mongoose.Types.ObjectId[];
+    rooms: mongoose.Types.ObjectId[];
+}
+
+const User = new mongoose.Schema<IUser>({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+    contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "Rooms" }],
 });
 
 export default mongoose.model("User", User);
