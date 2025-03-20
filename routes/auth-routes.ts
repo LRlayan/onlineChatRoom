eef2536c9aaa80541c2cf166db42aa174c867ffd
LRLayan <rameshlayan4@gmail.com>
2025-03-20 23:06:27 +0530

    register

--- routes/auth-routes.ts
+++ routes/auth-routes.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import {UserModel} from "../model/user-model";
 import jwt, {Secret} from "jsonwebtoken";
-import {verifyUserCredentialsService} from "../service/user-service";
+import {saveUserService, verifyUserCredentialsService} from "../service/user-service";
 
 dotenv.config();
 
@@ -26,6 +26,21 @@ router.post("/login", async (req: express.Request, res: express.Response) => {
     } catch (e) {
         console.log(e);
         res.status(400).send(e);
+    }
+});
+
+router.post("/register", async (req: express.Request, res: express.Response) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = new UserModel(username, email, password);
+    try {
+        const registration = await saveUserService(user);
+        res.status(201).json(registration);
+    } catch (e) {
+        console.log(e);
+        res.status(401).json(e);
     }
 });
 
