cfdf0c776b0ab7fb66d0ba6a1d3c82f59d678932
LRLayan <rameshlayan4@gmail.com>
2025-03-22 02:18:26 +0530

    add public

--- src/model/room.ts
+++ src/model/room.ts
@@ -1,10 +1,10 @@
 import {Contact} from "./contact.ts";
 
 export class Room {
-    name: string;
-    createAt: number | undefined;
-    image: File | string | null;
-    members: Contact[];
+    public name: string;
+    public createAt: number | undefined;
+    public image: File | string | null;
+    public members: Contact[];
 
     constructor(name: string, createAt: number | undefined, image: File | string | null, members: Contact[]) {
         this.name = name;
