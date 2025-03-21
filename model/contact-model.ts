cbee9960ad0b6b2d991ccbb06c634578ebb606d0
LRLayan <rameshlayan4@gmail.com>
2025-03-21 15:39:52 +0530

    contact model

---
+++ model/contact-model.ts
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+export class ContactModel {
+    private _code: string;
+    private _firstName: string;
+    private _lastName: string;
+    private _email: string;
+    private _rooms: string[];
+    private _users: string[];
+
+    constructor(code: string, firstName: string, lastName: string, email: string, rooms: string[], users: string[]) {
+        this._code = code;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._email = email;
+        this._rooms = rooms;
+        this._users = users;
+    }
+
+    get code(): string {
+        return this._code;
+    }
+
+    set code(value: string) {
+        this._code = value;
+    }
+
+    get firstName(): string {
+        return this._firstName;
+    }
+
+    set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    get lastName(): string {
+        return this._lastName;
+    }
+
+    set lastName(value: string) {
+        this._lastName = value;
+    }
+
+    get email(): string {
+        return this._email;
+    }
+
+    set email(value: string) {
+        this._email = value;
+    }
+
+    get rooms(): string[] {
+        return this._rooms;
+    }
+
+    set rooms(value: string[]) {
+        this._rooms = value;
+    }
+
+    get users(): string[] {
+        return this._users;
+    }
+
+    set users(value: string[]) {
+        this._users = value;
+    }
+}
