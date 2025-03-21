5eb0a0fbdc14bb4f540e4957c0eb8840b625877b
LRLayan <rameshlayan4@gmail.com>
2025-03-21 15:47:21 +0530

    remove properties

--- src/model/contact.ts
+++ src/model/contact.ts
@@ -2,14 +2,10 @@ export class Contact {
     public firstName: string;
     public lastName: string;
     public email: string;
-    public bio: string;
-    public profile: string;
 
-    constructor(firstName: string, lastName: string, email: string, bio: string, profile: string) {
+    constructor(firstName: string, lastName: string, email: string) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.bio = bio;
-        this.profile = profile;
     }
 }
