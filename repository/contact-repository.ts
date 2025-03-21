fd9acfcb2c8c27ae7ddd6b8d2dac987c231de0d6
LRLayan <rameshlayan4@gmail.com>
2025-03-21 15:39:41 +0530

    saveContactRepository

--- repository/contact-repository.ts
+++ repository/contact-repository.ts
@@ -1,6 +1,31 @@
 import mongoose from "mongoose";
 import Contacts from "../schema/contacts";
 
+interface Contacts {
+    code: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    rooms: string[];
+    users: string[];
+}
+
+export async function saveContactRepository(contactData: Contacts) {
+    try {
+        const newContact = new Contacts(contactData);
+        const response = newContact.save();
+        if (response) {
+            return response;
+        } else {
+            return { message: "Failed to add to contact. Please try again."}
+            throw new Error("Failed to add to contact. Please try again.");
+        }
+    } catch (e) {
+        console.error("Failed to add to contact:", e);
+        throw e;
+    }
+}
+
 export async function getSelectedContacts(_ids: mongoose.Types.ObjectId[]) {
     try {
         return await Contacts.find({ _id: { $in: _ids }});
