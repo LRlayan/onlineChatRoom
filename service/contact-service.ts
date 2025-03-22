27e514a72eaa66275d35ec4cf781389b7aa506c7
LRLayan <rameshlayan4@gmail.com>
2025-03-22 19:15:30 +0530

    getSelectedUsers

--- service/contact-service.ts
+++ service/contact-service.ts
@@ -1,15 +1,23 @@
 import {ContactModel} from "../model/contact-model";
 import Contacts from "../schema/contacts";
-import {findUserByEmail} from "../repository/user-repository";
+import {findUserByEmail, getSelectedUsers} from "../repository/user-repository";
 import {saveContactRepository} from "../repository/contact-repository";
+import mongoose from "mongoose";
+import User from "../schema/user";
 
-export async function saveContactService(contactData: ContactModel) {
+export async function saveContactService(loggedInUser: any ,contactData: ContactModel) {
     try {
+        let userRefObjectId: mongoose.Types.ObjectId[] = [];
+        let usernames: string[];
+
         const excitingUser = await findUserByEmail(contactData.email);
 
         if (!excitingUser) {
             return false;
         }
+
+        const userDocs = await User.find({ code: { $in: contactData.users }}).lean<{ _id: mongoose.Types.ObjectId }[]>();
+        userRefObjectId = userDocs.map((user) => user._id);
 
         const newContact = new Contacts({
             contactCode: contactData.code,
@@ -17,9 +25,17 @@ export async function saveContactService(contactData: ContactModel) {
             lastName: contactData.lastName,
             email: contactData.email,
             rooms: [],
-            users: []
+            users: [userRefObjectId]
         });
-        return await saveContactRepository(newContact);
+        const response = await saveContactRepository(newContact);
+        const getUsers = await getSelectedUsers(response.users);
+        usernames = getUsers.map((user) => user.username);
+
+        const modifyResponse = {
+            ...response.toObject(),
+            users: usernames
+        }
+        return modifyResponse;
     } catch (e) {
         console.error("Service layer error: Failed to add to contact!", e);
     }
