74d40653675799091da57b9d3a91ec4c1559772a
LRLayan <rameshlayan4@gmail.com>
2025-04-05 18:54:47 +0530

    getAllContactService

--- service/contact-service.ts
+++ service/contact-service.ts
@@ -1,7 +1,7 @@
 import {ContactModel} from "../model/contact-model";
 import Contacts from "../schema/contacts";
 import {findUserByEmail, getSelectedUsers} from "../repository/user-repository";
-import {saveContactRepository} from "../repository/contact-repository";
+import {getAllContacts, saveContactRepository} from "../repository/contact-repository";
 import mongoose from "mongoose";
 import User from "../schema/user";
 
@@ -16,16 +16,16 @@ export async function saveContactService(loggedInUser: any ,contactData: Contact
             return false;
         }
 
-        const userDocs = await User.find({ code: { $in: contactData.users }}).lean<{ _id: mongoose.Types.ObjectId }[]>();
+        const userDocs = await User.find({ username: { $in: loggedInUser }}).lean<{ _id: mongoose.Types.ObjectId }[]>();
         userRefObjectId = userDocs.map((user) => user._id);
 
         const newContact = new Contacts({
-            contactCode: contactData.code,
+            code: contactData.code,
             firstName: contactData.firstName,
             lastName: contactData.lastName,
             email: contactData.email,
             rooms: [],
-            users: [userRefObjectId]
+            users: userRefObjectId
         });
         const response = await saveContactRepository(newContact);
         const getUsers = await getSelectedUsers(response.users);
@@ -38,5 +38,14 @@ export async function saveContactService(loggedInUser: any ,contactData: Contact
         return modifyResponse;
     } catch (e) {
         console.error("Service layer error: Failed to add to contact!", e);
+    }
+}
+
+export async function getAllContactService() {
+    try {
+        return await getAllContacts();
+    } catch (e) {
+        console.error("Service layer error: Failed to get contact!", e);
+        throw new Error("Failed to get contact.");
     }
 }
