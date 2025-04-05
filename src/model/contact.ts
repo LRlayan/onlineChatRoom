fb9208b4a1d62f9039b154af46d74a4bacd8c2fd
LRLayan <rameshlayan4@gmail.com>
2025-04-05 18:26:58 +0530

    modify contact modal

--- src/model/contact.ts
+++ src/model/contact.ts
@@ -1,11 +1,21 @@
 export class Contact {
+    public code: string;
     public firstName: string;
     public lastName: string;
     public email: string;
+    public bio: string;
+    public image:File | null | undefined;
+    public rooms: string[];
+    public users: any;
 
-    constructor(firstName: string, lastName: string, email: string) {
+    constructor(code: string, firstName: string, lastName: string, email: string, bio: string, image: File | null | undefined, rooms: string[], users: any) {
+        this.code = code;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.bio = bio;
+        this.image = image;
+        this.rooms = rooms;
+        this.users = users;
     }
 }
